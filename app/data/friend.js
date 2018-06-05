ba17816b17589bac3913b9aa6358f66acb1b487b
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-06-04 22:09:35 -0700

    initial working

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Niyati Shah',
+      profilePic: 'https://imgur.com/a/YSqgiNS',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
